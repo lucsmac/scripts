aeaef772db9f966f37fd8ccfdd170b297e4f28eb
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-23 12:43:29 -0300

    feat: change wpp text
    Mudar o texto dos itens de wpp do dropdown do header no showroom 3.0

---
+++ 2021/marco/23/mudar-formato-dos-numeros/index.js
@@ -0,0 +1,26 @@
+(() => {
+  const changeWppText = ({ textToMatch, textToChange, textToInsert }) => {
+    const wppPhonesList = document.querySelectorAll('#header-card-whatsapp .list.list--header-phones .header-mobile__whatsapp-number')
+    const wppPhonesListMobile = document.querySelectorAll('#wpp-content-mobile-0 ul .header-mobile__whatsapp-number')
+
+    const targetsDesktop = Array.prototype.filter.call(wppPhonesList, (wpp) => {
+      return wpp.innerHTML.includes(textToMatch)
+    })
+
+    const targetsMobile = Array.prototype.filter.call(wppPhonesListMobile, (wpp) => {
+      return wpp.innerHTML.includes(textToMatch)
+    })
+
+    const targets = targetsDesktop.concat(targetsMobile)
+
+    Array.prototype.find.call(targets, target => {
+      target.innerText = target.innerText.replace(textToChange, textToInsert)
+    })
+  }
+
+  changeWppText({
+    textToMatch: '3864',
+    textToChange: '+55 (19)',
+    textToInsert: ''
+  })
+})()
